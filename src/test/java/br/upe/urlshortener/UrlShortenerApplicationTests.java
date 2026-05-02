e1ec32f931719b2e4e6b1f77b4507f8ee3dc9565
jc160 <jc16032005@gmail.com>
2026-05-02 12:32:16 -0300

    Feature: Swagger funcional

---
+++ src/test/java/br/upe/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
